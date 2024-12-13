7fdc7ea6a060ab8a2b0a305fe47e14ff0ace4a24
YiFan <yifan.lei@terapines.com>
2024-12-13 12:17:43 +0800

    Clean env-tpt.sh

--- env-tpt.sh
+++ env-tpt.sh
@@ -1,29 +1,12 @@
 #
 # Copyright (c) 2021 hpmicro
+# Copyright (C) 2020-2024 Terapines Technology (Wuhan) Co., Ltd
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
 
-#Terapines
-#export GNURISCV_TOOLCHAIN_PATH=/opt/zcc-toolchain/3.2.1
-#export HPM_SDK_TOOLCHAIN_VARIANT=zcc
-#export HPM_SDK_BASE=$HOME/work/code/git/hpm_sdk
-
-#export GNURISCV_TOOLCHAIN_PATH=/opt/zcc
-#export HPM_SDK_TOOLCHAIN_VARIANT=zcc
-#export HPM_SDK_BASE=$HOME/work/code/git/hpm_sdk
-
-#export GNURISCV_TOOLCHAIN_PATH=/opt/zcc-pro-u22
-#export HPM_SDK_TOOLCHAIN_VARIANT=zcc
-#export HPM_SDK_BASE=$HOME/work/code/git/hpm_sdk
-
-export GNURISCV_TOOLCHAIN_PATH=/home/crux/workspace/test_suit/install-zcc
+export ZCC_TOOLCHAIN_PATH=
 export HPM_SDK_TOOLCHAIN_VARIANT=zcc
-
-#Andes
-#export GNURISCV_TOOLCHAIN_PATH=/opt/andes-toolchain/v520/nds32le-elf-newlib-v5d
-#export HPM_SDK_TOOLCHAIN_VARIANT=nds-gcc
-#export HPM_SDK_BASE=$HOME/work/code/git/hpm_sdk
 
 if [ "X$MSYSTEM" "==" "X" ]; then
     if [ "X$name" "==" "Xenv.sh" ]; then
@@ -44,7 +27,7 @@ fi
 
 echo $HPM_SDK_BASE
 
-echo "GNURISCV_TOOLCHAIN_PATH=$GNURISCV_TOOLCHAIN_PATH"
+echo "GNURISCV_TOOLCHAIN_PATH=$ZCC_TOOLCHAIN_PATH"
 echo "HPM_SDK_TOOLCHAIN_VARIANT=$HPM_SDK_TOOLCHAIN_VARIANT"
 echo "HPM_SDK_BASE=$HPM_SDK_BASE"
 
